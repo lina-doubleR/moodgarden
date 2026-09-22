d1fb2bd38c3229d03ccdb70da4a6c33a615c7822
Полина <bwtuterus@gmail.com>
2026-09-22 21:54:08 +0500

    Сделала  HTML-страницу розовую с приветствием

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return "Привет! Это Mood Garden 🌸"
+    return render_template("home.html")
 
 
 if __name__ == "__main__":
